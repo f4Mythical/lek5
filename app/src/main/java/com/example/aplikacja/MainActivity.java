88da7ee46d56dffe5ba072a47b5bfbec77456b71
f4Mythical <grynka.adam@gmail.com>
2025-11-04 08:44:56 +0100

    Przelacznik i wsadzenie danych

--- app/src/main/java/com/example/aplikacja/MainActivity.java
+++ app/src/main/java/com/example/aplikacja/MainActivity.java
@@ -1,25 +1,45 @@
 package com.example.aplikacja;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Switch;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private Button przyciskPrev, przyciskNext;
+    private EditText poleNumer;
+    private Switch przelacznikTlo;
+    private ImageView obrazek;
+    private LinearLayout glowneTlo;
+    private int[] obrazy = {R.drawable.kot1, R.drawable.kot2, R.drawable.kot3, R.drawable.kot4};
+    private int aktualny = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        glowneTlo = findViewById(R.id.main);
+        obrazek = findViewById(R.id.imageView2);
+        przyciskPrev = findViewById(R.id.buttonPrev);
+        przyciskNext = findViewById(R.id.buttonNext);
+        poleNumer = findViewById(R.id.ImageNumber);
+        przelacznikTlo = findViewById(R.id.switchBlue);
+
+
+
+        przelacznikTlo.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                glowneTlo.setBackgroundColor(Color.parseColor("#1565c0"));
+            } else {
+                glowneTlo.setBackgroundColor(Color.parseColor("#00796B"));
+            }
         });
     }
 }
